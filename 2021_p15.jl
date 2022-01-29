ba811ea0acf3527250033d52e3449ed7e74dba55
Angel Brisco <angelbrisco@gmail.com>
2022-01-29 09:58:11 -0300

    puzzle 15_2

--- 2021_p15.jl
+++ 2021_p15.jl
@@ -50,65 +50,28 @@ function djt_step(known,explored,mat,djt)
 end
 
 #Part1
-"""while !("$sq_s,$sq_s" in explored)
+while !("$sq_s,$sq_s" in explored)
     djt_step(known,explored,mat,djt)
 end
+println(djt["$sq_s,$sq_s"])
 
-println(djt["$sq_s,$sq_s"])"""
+mat = Int16.(mat)
 
+mat2 = fill(Int16(0),size(mat).*5)
 
-function djt_extend_step(known,explored,mat,djt)
-    min_k=known[1]
-    ml = size(mat)[1]
-    for k in known
-        (djt[min_k][1]>djt[k][1]) && (min_k=k)
-    end
-    r,c=parse.(Int32,split(min_k,","))
-    r2,c2 = r,c
-    v_ad=0
-    while r2>ml
-        r2-=ml
-        v_ad+=1
-    end
-    while c2>ml
-        c2-=ml
-        v_ad+=1
-    end
-    """if (r!=1)&!("$(r-1),$c" in explored)
-        (r2==1) ? (mat_v = mat[10,c2]+v_ad-1) : (mat_v = mat[r2-1,c2]+v_ad)
-        while mat_v>9
-            mat_v-=9
-        end
-        (mat_v+djt[min_k][1]<djt["$(r-1),$c"][1]) && (djt["$(r-1),$c"]=[mat_v+djt[min_k][1],min_k])
-        !("$(r-1),$c" in known) && (push!(known,"$(r-1),$c"))
-    end
-    if (c!=1)&!("$r,$(c-1)" in explored)
-        (c2==1) ? (mat_v = mat[r2,10]+v_ad-1) : (mat_v = mat[r2,c2-1]+v_ad)
-        while mat_v>9
-            mat_v-=9
-        end
-        (mat_v+djt[min_k][1]<djt["$r,$(c-1)"][1]) && (djt["$r,$(c-1)"]=[mat_v+djt[min_k][1],min_k])
-        !("$r,$(c-1)" in known) && (push!(known,"$r,$(c-1)"))
-    end"""
-    if (r!=ml*5)&!("$(r+1),$c" in explored)
-        (r2==ml) ? (mat_v = mat[1,c2]+v_ad+1) : (mat_v = mat[r2+1,c2]+v_ad)
-        while mat_v>9
-            mat_v-=9
-        end
-        (mat_v+djt[min_k][1]<djt["$(r+1),$c"][1]) && (djt["$(r+1),$c"]=[mat_v+djt[min_k][1],min_k])
-        !("$(r+1),$c" in known) && (push!(known,"$(r+1),$c"))
-    end
-    if (c!=ml*5)&!("$r,$(c+1)" in explored)
-        (c2==ml) ? (mat_v = mat[r2,1]+v_ad+1) : (mat_v = mat[r2,c2+1]+v_ad)
-        while mat_v>9
-            mat_v-=9
-        end
-        (mat_v+djt[min_k][1]<djt["$r,$(c+1)"][1]) && (djt["$r,$(c+1)"]=[mat_v+djt[min_k][1],min_k])
-        !("$r,$(c+1)" in known) && (push!(known,"$r,$(c+1)"))
-    end
-    push!(explored,min_k)
-    filter!(x-> x!=min_k,known)
-end
+mat2[1:sq_s,1:sq_s].=mat.+0
+mat2[1:sq_s,(sq_s+1):(sq_s*2)].=mat.+1
+mat2[1:sq_s,(sq_s*2+1):(sq_s*3)].=mat.+2
+mat2[1:sq_s,(sq_s*3+1):(sq_s*4)].=mat.+3
+mat2[1:sq_s,(sq_s*4+1):(sq_s*5)].=mat.+4
+
+mat2[sq_s+1:(sq_s*2),:].=mat2[1:sq_s,:].+1
+mat2[(sq_s*2)+1:(sq_s*3),:].=mat2[sq_s+1:(sq_s*2),:].+1
+mat2[(sq_s*3)+1:(sq_s*4),:].=mat2[(sq_s*2)+1:(sq_s*3),:].+1
+mat2[(sq_s*4)+1:(sq_s*5),:].=mat2[(sq_s*3)+1:(sq_s*4),:].+1
+
+mat2 .= mat2.%9
+mat2 .+= (mat2.==0).*9
 
 djt=Dict()
 
@@ -117,13 +80,10 @@ for c in 1:(sq_s*5), r in 1:(sq_s*5)
 end
 djt["1,1"]=[0,"1,1"]
 
-
 explored=[]
 known=["1,1"]
 
-#Part2
 while !("$(sq_s*5),$(sq_s*5)" in explored)
-    djt_extend_step(known,explored,mat,djt)
+    djt_step(known,explored,mat2,djt)
 end
-
 println(djt["$(sq_s*5),$(sq_s*5)"])
