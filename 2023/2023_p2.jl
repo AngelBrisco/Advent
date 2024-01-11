799130be2b4b3ae8e9d6c450a37aa8947863695d
AngelBrisco <angelbrisco@gmail.com>
2024-01-10 22:01:07 -0300

    10/01/24

--- 2023/2023_p2.jl
+++ 2023/2023_p2.jl
@@ -36,3 +36,32 @@ for i in full_eval.(s, 12, 13, 14)
     (i[2]==1)&&(out+=i[1])
 end
 out
+
+#part 2
+
+function val_col(game_data)
+    throws = split(game_data,",")
+    throws = split.(throws, " ")
+    red_val = Int32(0)
+    green_val = Int32(0)
+    blue_val = Int32(0)
+    for t in throws
+        (t[3]=="red")&&(red_val = parse(Int32,t[2]))
+        (t[3]=="green")&&(green_val = parse(Int32,t[2]))
+        (t[3]=="blue")&&(blue_val = parse(Int32,t[2]))
+    end
+    [red_val, green_val, blue_val]
+end
+
+val_col(data_extraction(s[1])[2][1])
+
+function biggest_val_col(game_data)
+    maximum(hcat(val_col.(game_data)...),dims=2)
+end
+
+biggest_val_col(data_extraction.(s[5])[2])
+out2=0
+for g in data_extraction.(s)
+    out2 += reduce(*,biggest_val_col(g[2]))
+end
+out2
